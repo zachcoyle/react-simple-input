74420bd3b7d9adedc873e889636d466c4e4ae8d3
Zach Coyle <zach.coyle@gmail.com>
2017-05-30 22:12:13 -0400

    bust out components a little better

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,29 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-import Example from '../../src'
+import Input from '../../src';
 
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>react-simple-input Demo</h1>
-      <Example/>
-    </div>
-  }
-}
 
-render(<Demo/>, document.querySelector('#demo'))
+const FlexWrapper = ({ children }) =>
+  <div style={{
+    fontFamily: 'helvetica',
+    height: '100%',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  }}>
+    {children}
+  </div>
+
+
+const Demo = ({ props }) =>
+  <FlexWrapper>
+    <h1>react-simple-input Gallery</h1>
+    <Input name="hi" label="Label:" value="green" validator={v => v === "hello world" ? ({}) : ({ error: "error" })} />
+    <Input type="color" name="color" label="Input a Color:" />
+  </FlexWrapper>
+
+
+render(<Demo/>, document.querySelector('#demo'));
--- src/Input.js
+++ src/Input.js
@@ -1,29 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withState } from 'recompose';
 
-const DefaultValidationComponent = ({ validation }) => console.log(validation) ||
-  <div style={{ color: 'red' }}>{validation.error}</div>
+
+const DefaultValidationComponent = ({ validator, value }) =>
+  <div>
+    {value && validator && <div style={{ color: 'red' }}>{validator(value).error}</div>}
+  </div>
+
 
 const DefaultInputComponent = ({ type, name, value, onChange }) =>
   <input type={type} name={name} onChange={onChange} />
 
+
 const DefaultLabelComponent = ({ htmlFor, children }) =>
-  <label style={{ marginRight: '10px' }} htmlFor={name}>
-    {children}
-  </label>
+  <div>
+    {children &&
+      <label style={{ marginRight: '10px' }} htmlFor={name}>{children}</label>}
+  </div>
+
+
+const flexStyle = {
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+}
+
+const DefaultInputWrapperComponent = ({ name, InputComponent, LabelComponent, ValidationComponent }) =>
+  <div style={{ ...flexStyle, flexDirection: 'column' }}>
+    <div style={{ ...flexStyle, margin: 15 }}>
+      <LabelComponent />
+      <InputComponent />
+    </div>
+    <ValidationComponent />
+  </div>
+
+
+const onInputChange = ({ setDirty, onChange, setValue, setPristine }) => ({ target: { value } }) => {
+  console.log(value)
+  setDirty(true)
+  setPristine(false)
+  setValue(value)
+  onChange(value)
+}
 
 
 const Input = ({
   InputComponent=DefaultInputComponent,
   LabelComponent=DefaultLabelComponent,
   ValidationComponent=DefaultValidationComponent,
-  name, label, type, value, validationText, validator, mask,
+  InputWrapperComponent=DefaultInputWrapperComponent,
+  name,
+  label,
+  type="text",
+  value,
+  setValue,
+  validationText,
+  validator,
+  mask,
+  dirty,
+  setDirty,
+  pristine,
+  setPristine,
   onChange=() => {},
 }) => {
 
   InputComponent.propTypes = {
     name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
+    type: PropTypes.string,
     value: PropTypes.string,
     onChange: PropTypes.func,
   }
@@ -36,17 +81,17 @@ const Input = ({
     validation: PropTypes.object.isRequired,
   }
 
-  return <div>
-    {label && <LabelComponent htmlFor={name}>{label}</LabelComponent>}
-    <InputComponent name={name} type={type} value={value} onChange={onChange} />
-    {value && validator && <ValidationComponent validation={validator(value)} />}
-  </div>
-
+  return <InputWrapperComponent
+    LabelComponent={({...rest}) => <LabelComponent htmlFor={name} {...rest}>{label}</LabelComponent>}
+    InputComponent={({...rest}) => <InputComponent
+                                     name={name}
+                                     type={type}
+                                     value={value}
+                                     onChange={onInputChange({ setDirty, onChange, setValue, setPristine })}
+                                     {...rest} />}
+    ValidationComponent={({...rest}) => <ValidationComponent validator={validator} value={value} />}
+  />
 }
-
-
-export const ColorInput = (props) =>
-  <Input {...props} type="color" />
 
 
 Input.propTypes = {
@@ -59,4 +104,10 @@ Input.propTypes = {
 }
 
 
-export default Input;
+const withInputState = (Component) =>
+  withState('value', 'setValue', undefined)(
+  withState('dirty', 'setDirty', false)(
+  withState('pristine', 'setPristine', true)(
+    Component)))
+
+export default withInputState(Input);
